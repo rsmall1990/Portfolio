a5693b5d8bd36c56a9a316020596173332244cfa
Ryan Small <ryan.small@feapc.com>
2022-03-30 19:29:45 -0400

    wip: folder structure

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './App.css';
 
+import {About, Footer, Header, Skills, Testimonials, Work} from './container';
+import {Navbar} from './components';
+
 function App() {
   return (
-    <h1 className="App">App</h1>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
   );
 }
 
