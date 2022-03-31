daf47ee24941198799ee0d54ca59e62cd3e5f2d9
Ryan Small <ryan.small@feapc.com>
2022-03-31 08:48:04 -0400

    wip: adding images

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import {About, Footer, Header, Skills, Testimonials, Work} from './container';
 import {Navbar} from './components';
+import './App.scss';
 
 function App() {
   return (
